4515d89c2c6d919929b8d5fcad1bc6c5c451da28
Martin Neumann <martin.neumann@asti.tech>
2022-01-03 21:34:20 +0100

    feat: first features

---
+++ src/code.js
@@ -0,0 +1,94 @@
+// Globals
+var chart
+
+// Colors
+var rad = document.colors.color;
+var prev = null;
+for (var choice of rad) {
+	choice.addEventListener("click", function () {
+		console.log(this.value);
+		data.datasets[0].backgroundColor = this.value;
+		data.datasets[0].borderColor = this.value;
+		update();
+	});
+}
+
+// Pie
+var radPie = document.types.type;
+var prevPie = null;
+for (var choicePie of radPie) {
+	choicePie.addEventListener("click", function () {
+		console.log(this.value);
+		chart.destroy();
+		var ctx = document.getElementById('easygraphChart').getContext('2d'); // 2d context
+		chart = new Chart(
+			ctx,
+			{
+				type: this.value,
+				data: data,
+				options: options
+			}
+		);
+		update();
+	});
+}
+
+
+const labels = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+];
+
+const data = {
+	labels: labels,
+	datasets: [{
+		label: 'My First dataset',
+		backgroundColor: 'rgb(255, 99, 132)',
+		borderColor: 'rgb(255, 99, 132)',
+		data: [0, 10, 5, 2, 20, 30, 45],
+	}]
+};
+
+const options = {
+		responsive: true,
+		title: {
+			display: true,
+			text: 'Chart.js Line Chart'
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		backgroundColor: 'rgb(255, 99, 132)',
+};
+
+const config = {
+	type: 'line',
+	data: data,
+	options: options
+	
+};
+
+function init() {
+	var ctx = document.getElementById('easygraphChart').getContext('2d'); // 2d context
+
+	chart = new Chart(
+		ctx,
+		config
+	);
+}
+
+function update() {
+	chart.update();
+}
+
+
+window.onload = init();
