d1d16675270c5b6175d7a885bca27cc06001b07a
Martin Neumann <martin.neumann@asti.tech>
2022-01-31 23:21:04 +0100

    add some features

--- code.js
+++ code.js
@@ -13,6 +13,16 @@ for (var choice of rad) {
 	});
 }
 
+// Stacked
+var stacked = document.stacked;
+for (var choice of stacked) {
+	choice.addEventListener("click", function () {
+		console.log(this.value);
+		options.scales.y.stacked = this.value;
+		update();
+	});
+}
+
 // Pie
 var radPie = document.types.type;
 var prevPie = null;
@@ -33,8 +43,60 @@ for (var choicePie of radPie) {
 	});
 }
 
+// Render data list to form input
+function renderData() {
+	var dataList = document.getElementById("dataList");
+	var data = chart.data;
+	var dataStr = "";
+	for (var i = 0; i < data.datasets[0].data.length; i++) {
+		// Add a number input for each value and a minus button next to it for removal
+		dataList.innerHTML += "<div class='data-item'><input type='number' onchange=updateData() value='" + data.datasets[0].data[i] + "'><button class='btn btn-danger' onclick='removeData(" + i + ")'>-</button></div>";
+	}
+}
 
-const labels = [
+// Removing data
+function removeData(i) {
+	var dataList = document.getElementById("dataList");
+	var data = chart.data;
+	data.datasets[0].data.splice(i, 1);
+	dataList.innerHTML = "";
+	labels.splice(i, 1);
+	renderData();
+	update();
+}
+
+// Adding data
+function addDataToDataset() {
+	var dataList = document.getElementById("dataList");
+	var dataToAdd = document.getElementById("dataFieldX").value;
+	data.datasets[0].data.push(parseInt(dataToAdd));
+	dataToAdd = document.getElementById("dataFieldY").value;
+	labels.push(dataToAdd);
+	dataList.innerHTML = "";
+	renderData();
+	update();
+}
+
+// Update changed data
+function updateData() {
+	console.log("updateData");
+	var dataList = document.getElementById("dataList");
+	update();
+}
+
+// Update chart
+function resetGraph() {
+	var dataList = document.getElementById("dataList");
+	data.datasets[0].data = [];
+	labels = [];
+	dataList.innerHTML = "";
+	renderData();
+	update();
+}
+
+
+
+let labels = [
 	'January',
 	'February',
 	'March',
@@ -43,7 +105,7 @@ const labels = [
 	'June',
 ];
 
-const data = {
+let data = {
 	labels: labels,
 	datasets: [{
 		label: 'My First dataset',
@@ -54,27 +116,41 @@ const data = {
 };
 
 const options = {
-		responsive: true,
-		title: {
-			display: true,
-			text: 'Chart.js Line Chart'
-		},
-		tooltips: {
-			mode: 'index',
-			intersect: false,
-		},
-		hover: {
-			mode: 'nearest',
-			intersect: true
-		},
-		backgroundColor: 'rgb(255, 99, 132)',
+	responsive: true,
+	title: {
+		display: true,
+		text: 'Chart.js Line Chart'
+	},
+	tooltips: {
+		mode: 'index',
+		intersect: false,
+	},
+	hover: {
+		mode: 'nearest',
+		intersect: true
+	},
+	scales: {
+		x: {
+			title: {
+				display: true,
+				text: "X"
+			},
+			y: {
+				title: {
+					display: true,
+					text: "Y"
+				},
+			}
+		}
+	},
+	backgroundColor: 'rgb(255, 99, 132)',
 };
 
 const config = {
 	type: 'bar',
 	data: data,
 	options: options
-	
+
 };
 
 function init() {
@@ -84,6 +160,8 @@ function init() {
 		ctx,
 		config
 	);
+
+	renderData();
 }
 
 function update() {
